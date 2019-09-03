2d1a5597b68134ec981f09dde4f41000558b817a
Gustavo Morales <gustavo.morales@gmail.com>
2019-09-02 19:28:28 -0500

    Add DataTable Component and Remove Action

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
+import DataTable from './components/DataTable'
 
 export default class App extends Component {
+  state = {
+    tasks: [{
+      _id: '1',
+      title: 'Setup project',
+      projectId: {
+        title: 'Project 1'
+      },
+      userId: {
+        name: 'Gustavo Morales'
+      },
+      completed: false 
+    },{
+      _id: '2',
+      title: 'Layout',
+      projectId: {
+        title: 'Project 1'
+      },
+      userId: {
+        name: 'Gustavo Morales'
+      },
+      completed: true
+    }]
+  }
+
+  handleRemove = (id) => {
+    let tasks = [...this.state.tasks];
+    const updatedTasks = tasks.filter(task => task._id !== id);
+    this.setState({ tasks: updatedTasks});
+  }
+
   render() {
+    const { tasks } = this.state;
+    const count = tasks.length;
+
     return (
-      <div>
-        <h1>Konza</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <h2>Projects</h2>
+          </div>
+          <div className="col">
+            <h2>Tasks</h2>
+            <p>Displaying {count} items</p>
+            <DataTable items={tasks} remove={this.handleRemove} />
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/components/DataTable.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function DataTable(props) {
+  const { items, remove } = props;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Project</th>
+          <th scope="col">Title</th>
+          <th scope="col">Author</th>
+          <th scope="col">Completed</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        { items.map(item => 
+          <tr key={item._id}>
+            <td>{item.projectId.title}</td>
+            <td>{item.title}</td>
+            <td>{item.userId.name}</td>
+            <td>{item.completed ? "Completed" : "Pending"}</td>
+            <td>
+              <button 
+                className="btn btn-danger" 
+                onClick={() => remove(item._id)}
+              >
+                Remove
+              </button>
+            </td>
+          </tr>
+        ) }
+      </tbody>
+    </table>
+  )
+}
+
+
