9c7bb1008b71a83b0359cd284fdd04b9bdc6d7d5
Gustavo Morales <gustavo.morales@gmail.com>
2019-09-04 18:29:56 -0500

    Refactor App to prepare for Routing

--- src/App.js
+++ src/App.js
@@ -1,71 +1,8 @@
-import React, { Component } from 'react';
-import http from './services/http';
-import config from './config.json';
-import TaskTable from './components/TaskTable';
-import Pagination from './common/Pagination';
-import ProjectList from './components/ProjectList';
+import React from 'react'
+import Tasks from './pages/Tasks'
 
-export default class App extends Component {
-  state = {
-    tasks: [],
-    page: 1,
-    pages: 1,
-    project: ''
-  }
-
-  componentDidMount() {
-    this.loadTasks();  
-  }
-
-  loadTasks = async (nextPage = 1) => {
-    const { data } = await http.get(`${config.baseUrl}/tasks?page=${nextPage}`);
-    const { data: tasks, meta: { page, pages } } = data;
-    
-    this.setState({ tasks, page, pages });
-  }
-
-  handleRemove = async (id) => {
-    try {
-      await http.delete(`${config.baseUrl}/tasks/${id}`);
-      this.loadTasks();
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  handleSelectPage = (nextPage) => {
-    this.loadTasks(nextPage);
-  }
-
-  handleSelectedProject = (project) => {
-    this.setState({ project });
-  }
-
-  render() {
-    const { tasks, page, pages, project } = this.state;
-
-    let filteredTasks = [...tasks];
-
-    if (project) {
-      filteredTasks = filteredTasks.filter(task => task.projectId._id === project)
-    }
-
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-4">
-            <h2>Projects</h2>
-            <ProjectList selected={project} onSelected={this.handleSelectedProject} />
-          </div>
-          <div className="col">
-            <h2>Tasks</h2>
-            <TaskTable tasks={filteredTasks} remove={this.handleRemove} />
-            <Pagination page={page} pages={pages} onSelectPage={this.handleSelectPage} />
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-
-};
+export default function App() {
+  return (
+    <Tasks />
+  )
+}
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,18 +6,6 @@ import List from '../common/List';
 export default class ProjectList extends Component {
   state = {
     projects: [],
-    selected: ''
-  }
-
-  static getDerivedStateFromProps(props, state) {
-    if (props.selected !== state.selected) {
-      return {
-        selected: props.selected,
-      };
-    }
-
-    // Return null if the state hasn't changed
-    return null;
   }
 
   async componentDidMount() {
@@ -30,19 +18,17 @@ export default class ProjectList extends Component {
     });
   }
 
-  handleSelected = (selected) => {
-    this.props.onSelected(selected);
-  }
-  
   render() {
-    const { projects, selected } = this.state;
+    const { projects } = this.state;
+    const { onSelected, selected } = this.props;
+
     return (
       <List 
         items={projects} 
         label="title" 
         dataKey="_id" 
         selected={selected} 
-        onSelected={this.handleSelected} 
+        onSelected={onSelected} 
       />
     )
   }
