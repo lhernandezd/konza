c687a3d41ec5e1fb5a5bd869ba46bd94ef6c4673
Gustavo Morales <gustavo.morales@gmail.com>
2019-09-05 04:27:26 -0500

    Add Config to Format files with Prettier

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import NavBar from './components/NavBar';
-import Tasks from './pages/Tasks';
-import Projects from './pages/Projects';
-import Users from './pages/Users';
+import NavBar from "./components/NavBar";
+import Tasks from "./pages/Tasks";
+import Projects from "./pages/Projects";
+import Users from "./pages/Users";
 
 export default function App() {
   return (
     <Fragment>
       <NavBar />
       <Switch>
-        <Route path="/tasks" render={(props) => <Tasks {...props} user="Gustavo" />} />
+        <Route
+          path="/tasks"
+          render={props => <Tasks {...props} user="Gustavo" />}
+        />
         <Route path="/projects/:id" component={Projects} />
         <Route path="/projects" component={Projects} />
         <Route path="/users" component={Users} />
         <Redirect to="/tasks" />
       </Switch>
     </Fragment>
-  )
+  );
 }
